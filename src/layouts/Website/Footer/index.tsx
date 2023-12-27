fab6e0276c6c4d007b59bcbbf80160c0f2223323
tam11a <ibrahimsadiktamim@gmail.com>
2023-12-27 16:15:36 +0600

    added: footer & icon

--- src/layouts/Website/Footer/index.tsx
+++ src/layouts/Website/Footer/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Icon } from "@iconify/react";
 
 const WebsiteFooter: React.FC = () => {
-	return <footer className="max-w-6xl mx-auto ">Footer</footer>;
+	return (
+		<footer className="max-w-6xl mx-auto border-t-primary-900 border-opacity-65 border-t pt-7 pb-10">
+			<div className="flex items-center gap-2 justify-center text-sm font-semibold tracking-widest text-slate-400">
+				Designed by{" "}
+				<Icon
+					icon="pixelarticons:heart"
+					className="text-primary"
+				/>{" "}
+				<span className="text-white">TAM</span>
+			</div>
+		</footer>
+	);
 };
 
 export default WebsiteFooter;
