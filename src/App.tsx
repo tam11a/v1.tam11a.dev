0eb329ea1f58312c5a89956a01da17a58ff7a444
tam11a <ibrahimsadiktamim@gmail.com>
2024-01-04 17:46:52 +0600

    added: firebase analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import BaseRoutes from "./routes";
+import firebaseConfig from "./config/firebase";
+import React from "react";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
+	React.useEffect(() => {
+		getAnalytics(app);
+	}, []);
 	return (
 		<>
 			<BaseRoutes />
---
+++ src/config/firebase.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyBkXTNpyJZ6K--k2_1jYLQzduhFnm8Uuqo",
+	authDomain: "tam11a.firebaseapp.com",
+	projectId: "tam11a",
+	storageBucket: "tam11a.appspot.com",
+	messagingSenderId: "12388468204",
+	appId: "1:12388468204:web:758cdd1470748f3063ecc9",
+	measurementId: "G-XZN6NQR09Q",
+};
+
+export default firebaseConfig;
